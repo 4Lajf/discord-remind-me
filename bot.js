783094f2f3fc76a95d36d974940cd5978c80b315
4Lajf <sajmon120xd@gmail.com>
2024-01-29 23:47:33 +0100

    git

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const { setupDb, addReminder, getReminders, removeReminder } = require('./database');
+const { setupDb, addReminder, getReminders, removeReminder, addStarredMessage, isMessageOnStarboard } = require('./database');
 const { generateReminderDate } = require('./utils/remindMeUtils');
 
 const token = process.env.TOKEN;
-const { Client } = require('discord.js-selfbot-v13');
+const { Client, MessageEmbed } = require('discord.js-selfbot-v13');
 const client = new Client();
 
 client.once('ready', async () => {
@@ -80,12 +80,135 @@ client.on('messageCreate', async message => {
     return;
   }
 
-  if (message.content.toLowerCase().includes('zbawić')) {
+  if (message.content.toLowerCase().includes('patriarchat')) {
     message.reply({
-      content: `Zniszczyć fandom`,
+      content: `https://cdn.discordapp.com/attachments/895595804276961301/1196458167748534332/image.webp?ex=65c0ee00&is=65ae7900&hm=eccca85fd3491ad49e460b3bc67bb1bc9c7c37f3c3b16cff4ee477cca26c1111&`,
       allowedMentions: { repliedUser: false }
     });
     return;
+  }
+
+  // if (message.content.toLowerCase().includes('polityka') || message.content.toLowerCase().includes('polityczny') || message.content.toLowerCase().includes('politycznie') || message.content.toLowerCase().includes('polityk')) {
+  //   message.reply({
+  //     content: `<#1165755063256494082>`,
+  //     allowedMentions: { repliedUser: false }
+  //   });
+  //   return;
+  // }
+
+  if (message.content.toLowerCase().startsWith('.ktopytal')) {
+    message.reply({
+      content: `https://cdn.discordapp.com/attachments/895385698981543999/1200552760811192461/xv8AmU7.png?ex=65c698e3&is=65b423e3&hm=1955da13b205bf858b385b75d30768f72dcb98331d2fb37768e531bca2cfea31&`,
+      allowedMentions: { repliedUser: false }
+    });
+    return;
+  }
+
+  if (message.content.toLowerCase().startsWith('.definicja')) {
+    message.reply({
+      content: `https://i.imgur.com/dCSkN2I.png`,
+      allowedMentions: { repliedUser: false }
+    });
+    return;
+  }
+
+  // if (message.content.toLowerCase().includes('zbawić')) {
+  //   message.reply({
+  //     content: `Zniszczyć fandom`,
+  //     allowedMentions: { repliedUser: false }
+  //   });
+  //   return;
+  // }
+
+  if (message.content.toLowerCase().includes('konwent rogera') || message.content.toLowerCase().includes('konwent rodzera') || message.content.toLowerCase().includes('konwent rodżera') || message.content.toLowerCase().includes('konwent rodgera')) {
+    message.reply({
+      content: `już nigdy się nie pozbiera`,
+      allowedMentions: { repliedUser: false }
+    });
+    return;
+  }
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+  // Check if the reaction is not from a bot
+  if (user.bot) return;
+
+  // Fetch the message if not cached
+  if (reaction.partial) {
+    try {
+      await reaction.fetch();
+    } catch (error) {
+      console.error('Fetching message failed:', error);
+      return;
+    }
+  }
+
+  const { message } = reaction;
+  if (message.channelId === '901928246231314472' || message.channelId === '902594330932432947' || message.channelId === '915604394031079465') return;
+  const starboardChannelID = "895593116109799434";
+  const specialReactionID = "906592134545690664";
+
+  // Check for 10 reactions of any kind or the specific reaction
+  if (reaction.count >= 10 || reaction.emoji.id === specialReactionID) {
+    const { message } = reaction;
+
+    // Check if the message has already been starred
+    const starboardMessage = await isMessageOnStarboard(message.id)
+    if (starboardMessage.length !== 0) return;
+
+    const authorID = message.author.id;
+    const authorAvatar = message.author.displayAvatarURL();
+    const messageLink = message.url;
+    const messageContent = message.content;
+
+    // // Create the embed
+    // const embed = new MessageEmbed()
+    //   .setAuthor({ name: authorName, iconURL: authorAvatar })
+    //   .setDescription(messageContent)
+    //   .setColor('#FFAC33');
+
+    // // Attach all message attachments as image links
+    // message.attachments.forEach((attachment) => {
+    //   embed.setImage(attachment.url);
+    // });
+
+    // Preparing 'similar to embed' message content with markdown
+    let simulatedEmbedContent = `:sparkles: <${authorID}> ➤ ${messageLink}\n\n>>> ${messageContent}`;
+
+    // Include attachments if any
+    if (messageContent) {
+      message.attachments.forEach((attachment) => {
+        simulatedEmbedContent += `\n\n${attachment.url}\n`;
+      });
+    } else {
+      message.attachments.forEach((attachment) => {
+        simulatedEmbedContent += `${attachment.url}\n`;
+      });
+    }
+
+
+    // Preparing 'similar to embed' message content with markdown
+    let simulatedEmbedContentFinal = `:sparkles: <@${authorID}> ➤ ${messageLink}\n\n>>> ${messageContent}`;
+
+    // Include attachments if any
+    if (messageContent) {
+      message.attachments.forEach((attachment) => {
+        simulatedEmbedContentFinal += `\n\n${attachment.url}\n`;
+      });
+    } else {
+      message.attachments.forEach((attachment) => {
+        simulatedEmbedContentFinal += `${attachment.url}\n`;
+      });
+    }
+
+    // Send to the starboard channel as a regular message
+    const starboardChannel = await client.channels.fetch(starboardChannelID);
+    if (starboardChannel) {
+      let message = await starboardChannel.send(simulatedEmbedContent);
+      await message.edit(simulatedEmbedContentFinal)
+    }
+    // Insert into database as starred
+    await addStarredMessage(message.id, message.channelId, true);
   }
 });
 
--- database.js
+++ database.js
@@ -1,15 +1,22 @@
 const sqlite = require('sqlite');
 const sqlite3 = require('sqlite3');
 
-async function openDb() {
+async function openRemiderDb() {
     return sqlite.open({
         filename: './reminderData.db',
         driver: sqlite3.Database
     });
 }
 
+async function openStarboardDb() {
+    return sqlite.open({
+        filename: './starboard.db',
+        driver: sqlite3.Database
+    });
+}
+
 async function setupDb() {
-    const db = await openDb();
+    const db = await openRemiderDb();
     await db.migrate({
         migrationsPath: './migrations', // This folder will store your migration files
         force: 'last',
@@ -18,20 +25,31 @@ async function setupDb() {
 }
 
 async function addReminder(userId, remindDate, message) {
-    const db = await openDb();
+    const db = await openRemiderDb();
     const stmt = await db.prepare('INSERT INTO reminders (userId, remindDate, message) VALUES (?, ?, ?)');
     await stmt.run(userId, remindDate, message);
     await stmt.finalize();
 }
 
 async function getReminders() {
-    const db = await openDb();
+    const db = await openRemiderDb();
     return db.all('SELECT * FROM reminders WHERE remindDate <= ?', new Date().toISOString());
 }
 
 async function removeReminder(id) {
-    const db = await openDb();
+    const db = await openRemiderDb();
     await db.run('DELETE FROM reminders WHERE id = ?', id);
 }
 
-module.exports = { setupDb, addReminder, getReminders, removeReminder };
+async function isMessageOnStarboard(messageId) {
+    const db = await openStarboardDb();
+    return db.all(`SELECT messageId FROM starred_messages WHERE messageId = ?`, messageId);
+}
+
+async function addStarredMessage(messageId, channelId, isStarred) {
+    const db = await openStarboardDb();
+    await db.run(`INSERT INTO starred_messages (messageId, channelId, starred) VALUES (?, ?, ?)`,
+        [messageId, channelId, isStarred]);
+}
+
+module.exports = { setupDb, addReminder, getReminders, removeReminder, addStarredMessage, isMessageOnStarboard };
